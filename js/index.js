7b6ab0edc442ea1db67de018acc606f83fdb06f1
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-06-18 15:54:59 -0500

    button color change on index.js.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const button1 = document.querySelector('btn1');
+button1.addEventListener('click', function(event){
+  event.target.style.color = 'red';
+});
+
+
+const button2 = document.querySelector('btn2');
+button1.addEventListener('click', function(event){
+  event.target.style.color = 'white';
+});
+
+
+const button3 = document.querySelector('btn3');
+button1.addEventListener('click', function(event){
+  event.target.style.color = 'blue';
+});
